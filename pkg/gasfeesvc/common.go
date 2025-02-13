5c7440b043c6b448f2d9430e9c9e1e6fa19f4ef5
Wind <4777457+windycrypto@users.noreply.github.com>
2025-02-13 06:21:35 +0300

    Improve Gasfee suggestion logic (#24)

--- pkg/gasfeesvc/common.go
+++ pkg/gasfeesvc/common.go
@@ -1,5 +1,7 @@
 package gasfeesvc
 
+import "math"
+
 type EstimatedGasFee struct {
 	MaxPriorityFeePerGas float64 `json:"maxPriorityFeePerGas"`
 	MaxFeePerGas         float64 `json:"maxFeePerGas"`
@@ -13,5 +15,13 @@ type SuggestedGasFees struct {
 	HistoricalRewards          []float64                   `json:"historicalRewards,omitempty"`
 	RegulatedHistoricalRewards []float64                   `json:"regulatedHistoricalRewards,omitempty"`
 	StdDevThreshold            float64                     `json:"stdDevThreshold,omitempty"`
+	PredictMode                string                      `json:"predictMode,omitempty"`
 	EstimatedGasFees           map[string]*EstimatedGasFee `json:"estimatedGasFees"`
+}
+
+// round rounds a float64 to the specified number of decimal places.
+func round9(val float64) float64 {
+	precision := 9
+	ratio := math.Pow(10, float64(precision))
+	return math.Round(val*ratio) / ratio
 }
